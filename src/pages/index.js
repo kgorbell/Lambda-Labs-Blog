59b6ec5ed2e9366741e39bb6af65aa8f69d34f74
Katie Gorbell <katiegorbell@gmail.com>
2018-11-09 13:36:30 -0600

    partially completed blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,14 @@ import Image from '../components/image'
 const IndexPage = () => (
   <Layout>
     <h1>Week 1</h1>
-    <p></p>
-    <p></p>
+    <h3>Individual Accomplishments:</h3>
+    <p> Most of my work this week was directed towards establishing a styling template for our React application that will allow for a fluid user experience. In order to allow for an easily manages styles library, I installed less into our project and included separate files for variables, and mixins. When applying these styled to our various components, we came to an agreement on a structure for each webpage and applied that to all the components we built. My Components contribution include the header, footer, navigation icon, and jumbotron (located on the landing page).</p>
+    <p> In addition to working on the Front-End, I took some time this week in order to learn about the GraphQL Yoga database. After reviewing how my fellow team members built out their end points, I assisted in getting our database deployed.</p>
+    <p>The following lists my contributions to the project:</p>
+    <p>FRONT END:</p>
+    <ul></ul>
+    <ul></ul>
+    <ul></ul>
   </Layout>
 )
 
