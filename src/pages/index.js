b4f4580b65a48d77f2b83216b7b91827e2d9e41c
Katie Gorbell <katiegorbell@gmail.com>
2018-11-09 12:42:33 -0600

    initial creation of repository

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Week 1</h1>
+    <p></p>
+    <p></p>
+  </Layout>
+)
+
+export default IndexPage
